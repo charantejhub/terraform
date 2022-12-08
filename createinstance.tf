eab113389a3e7e2eb41e4e94d3540aae059aad9c
charan <charanteja.yerrabala@gmail.com>
2022-12-07 23:34:25 -0500

    variables basics

--- createinstance.tf
+++ createinstance.tf
@@ -3,4 +3,7 @@
 resource "aws_instance" "my_instance" {
     ami           = "ami-0f91cedb707b09db0"
     instance_type = "t2.micro"
+
+    security_groups = var.securtiygroup
 }
+
--- variables.tf
+++ variables.tf
@@ -2,5 +2,9 @@ variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "us-east-2"
-  
+}
+
+variable "securtiygroup" {
+    type = list
+    default = ["sg-777","sg-888"] 
 }
