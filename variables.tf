6d0f231ceb9e9b64d545e1449cece3f91dfb3e50
charan <charanteja.yerrabala@gmail.com>
2022-12-08 12:21:31 -0500

    modifed varibale file

--- variables.tf
+++ variables.tf
@@ -8,3 +8,27 @@ variable "securtiygroup" {
     type = list
     default = ["sg-777","sg-888"] 
 }
+
+variable "AMIS" {
+    type = map
+    default = {
+        us-east-1 = "ami-0b0ea68c435eb488d"
+        us-east-2 = "ami-05803413c51f242b7"
+        us-west-1 = "ami-0454207e5367abf01"
+        us-west-2 = "ami-0688ba7eeeeefe3cd"
+    }
+  
+}
+variable "PATH_TO_PUBLIC_KEY" {
+    default="levelup_key.pub"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "levelup_key"
+  
+}
+
+variable "INSTANCE_USERNAME" {
+    default = "ubuntu"
+  
+}
