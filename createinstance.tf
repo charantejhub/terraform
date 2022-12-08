d73afcf5e193f3eb0e46f36338901db396d385bc
charan <charanteja.yerrabala@gmail.com>
2022-12-07 21:47:48 -0500

    variables demo

--- createinstance.tf
+++ createinstance.tf
@@ -1,10 +1,6 @@
-provider "aws" {
-    access_key = "ACCESS_KEYHERE"
-    secret_key = "SECRET_KEYHERE"
-    region     = "us-east-2"
-}
+
 
 resource "aws_instance" "my_instance" {
     ami           = "ami-0f91cedb707b09db0"
-    instance_type = t2.micro
+    instance_type = "t2.micro"
 }
